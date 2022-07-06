5d7af6d4c3e9dedcfa770f0acae6d6eb5c658cea
Vinicius Almeida <73404432+0almeida@users.noreply.github.com>
2022-07-06 23:07:51 +0000

    .

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-import teste.Curso;
+package teste;
 
 import java.io.IOException;
 import java.util.Scanner;
@@ -13,7 +13,60 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        //criar instancia das classes aqui e printar as mesmas;
+
+ 
+    public void construir(){
+
+          public class NovoCurso implements Curso{
+        @Override
+       NovoCurso c1 = new teste.Curso (String detalhes, Double preco);
+
+ 
+      System.out.printf("Digite o nome do curso");
+  detalhes = ler.next(); // 3. entrada de dados
+
+
+    System.out.printf("Digite o preço do curso");
+  preco = ler.next(); // 3. entrada de dados
+
     }
+    }
+
+    }
+
+ public class NovoCliente extends Cliente {
+
+ public NovoCliente(nome,email,telefone)
+    {
+         Scanner ler = new Scanner(System.string); // 2. instanciando e criando um objeto Scanner
+
+if( !"".equals(nome.getText())) {
+      //digite aqui a instrução se o campo for NÃO vazio e NÃO nulo.
+      System.out.printf("Digite o nome da empresa");
+  nome = ler.next(); // 3. entrada de dados
+
+
+    System.out.printf("Digite o e-mail da empresa");
+  email = ler.next(); // 3. entrada de dados
+
+    System.out.printf("Digite o telefone da empresa");
+  telefone= ler.next(); // 3. entrada de dados
+
+System.out.printf("\Confirmando os dados:\n");
+
+
+}else{
+  System.out.println("Dados Preenchidos:")
+      System.out.printf("Nome:", nome,"Email:", email,"Telefone:",telefone);
+
+}
+
+ cout << "Pressione qualquer tecla para continuar..." << endl;
+    getchar();
+    return 0;
+
+    }
+}
+
 
 }
